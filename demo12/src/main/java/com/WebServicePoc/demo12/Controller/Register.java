42b9a88b3093022615a1d4d154d1a5c3f009eab3
kirandagumati428 <kirandagumati428@gmail.com>
2022-07-11 18:04:56 +0530

    First commit

---
+++ demo12/src/main/java/com/WebServicePoc/demo12/Controller/Register.java
@@ -0,0 +1,39 @@
+package com.WebServicePoc.demo12.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/registation")
+public class Register {
+    @GetMapping("/userdetails")
+public String getUserDetails(){
+        return "I am get method";
+    }
+    @GetMapping("/detailsbyid/{id}")
+public  String getUserDetailsById(@PathVariable int id){
+        return "{\n" +
+                "    \"firstName\": \"John\",\n" +
+                "    \"lastName\": \"doe\",\n" +
+                "    \"age\": 26,\n" +
+                "    \"address\": {\n" +
+                "        \"streetAddress\": \"naist street\",\n" +
+                "        \"city\": \"Nara\",\n" +
+                "        \"postalCode\": \"630-0192\"\n" +
+                "    },\n" +
+                "    \"phoneNumbers\": [\n" +
+                "        {\n" +
+                "            \"type\": \"iPhone\",\n" +
+                "            \"number\": \"0123-4567-8888\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"type\": \"home\",\n" +
+                "            \"number\": \"0123-4567-8910\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}";
+    }
+}
+
